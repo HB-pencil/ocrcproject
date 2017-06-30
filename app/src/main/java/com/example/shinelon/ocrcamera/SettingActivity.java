d88ecc39c549c0eca4e2d227a88d3877157b0975
HB <HardBlack@126.com>
2017-06-30 22:03:06 +0800

    baiduapi基本Activity添加

---
+++ app/src/main/java/com/example/shinelon/ocrcamera/SettingActivity.java
@@ -0,0 +1,10 @@
+package com.example.shinelon.ocrcamera;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Shinelon on 2017/6/30.
+ */
+
+public class SettingActivity extends AppCompatActivity {
+}
